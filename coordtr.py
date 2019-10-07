2d318b4ac18ad90e8d8063a9b14bf11f03b475c7
Juan Bernales <juanbernales@gmail.com>
2019-10-07 01:16:10 -0400

    script

--- coordtr.py
+++ coordtr.py
@@ -1,1 +1,11 @@
-1
+from qgis._core import QgsCoordinateTransform, QgsCoordinateReferenceSystem,\
+    QgsProject
+from qgis.core import QgsPointXY
+x , y = -112.0, 13.0
+crs = QgsCoordinateReferenceSystem(4326) # wgs84
+crsnuevo = QgsCoordinateReferenceSystem(26912) # NAD 83 / UTM Zpne 12
+trform =  QgsCoordinateTransform(crs, crsnuevo, QgsProject.instance())
+ptutm = trform.transform(QgsPointXY(x , y))
+print("punto utm:", ptutm)
+ptwgs = trform.transform(ptutm, QgsCoordinateTransform.ReverseTransform)
+print("punto wgs:", ptwgs)
